01cfe8300f79e3fbfb8b25742171959c0f6a2c89
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-12-10 12:24:45 +0900

    refactor: organize files

--- vibe/add.js
+++ vibe/add.js
@@ -1,11 +1,11 @@
 import { globSync, readFileSync } from 'node:fs';
-import { collection } from '../client.js';
+import { collection } from '../chroma/client.js';
 
 for await (const path of globSync('./vibe/tracks/*.json')) {
 	const id = path.split('/').pop()?.replace('.json', '');
 	if (!id) continue;
 
-	/** @type {import('../../vibe/types.js').Track} */
+	/** @type {import('../vibe/types.js').Track} */
 	const { lyric, ...track } = JSON.parse(readFileSync(path, 'utf-8'));
 
 	if (!lyric.text) continue;
